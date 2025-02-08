6be83098a381a09145ffd659226a165f304cf48f
Yahya chohan <yahyachohan123@gmail.com>
2025-02-08 22:21:57 +0500

    Addfunction

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-let result = document.getElementById("result")
+let result = document.getElementById("result");
+
 function genotp() {
-    let otp = Math.round(Math.random()*1000000)
-result.innerText = `Your OTP Code is ${otp}` 
-console.log(otp)
+    // Get the values and convert them to numbers inside the function
+    let numOne = +document.getElementById("numOne").value;
+    let numTwo = +document.getElementById("numTwo").value;
+
+    console.log(typeof(numOne), typeof(numTwo)); // Check the types of numOne and numTwo
+
+    let div = numOne / numTwo;
+
+    if (numTwo === 0 || div === Infinity || div === -Infinity)  {
+        result.textContent = "Math Error";
+        console.log("Math Error");
+    } else {
+        result.textContent = Math.round(div) ;
+        console.log(div);
+    }
 }
+
 
 
